ede2ba714b670e8bdbd512b76b87a56825a6f7f1
Marius Kriegerowski <marius@gfz-potsdam.de>
2023-03-07 14:45:24 +0100

    test for empty sensor list

--- tests/test-client.py
+++ tests/test-client.py
@@ -10,16 +10,20 @@ def client():
     client = QSClient(
         email=os.environ.get("TEST_CLIENT_EMAIL"),
         password=os.environ.get("TEST_CLIENT_PASSWORD"),
-        base_domain=os.environ.get("TEST_CLIENT_DOMAIN")
+        base_domain=os.environ.get("TEST_CLIENT_DOMAIN"),
     )
     yield client
 
 
 @pytest.fixture
 def sensor(client):
-    sensor_uid = client.get_sensor_ids()[0]
-    logging.info(f"Test sensor: {sensor_uid}")
-    yield client.get_sensor(sensor_uid)
+    sensor_uids = client.get_sensor_ids()
+
+    if len(sensor_uids) == 0:
+        raise Exception("No sensors associated with this user account!")
+
+    logging.info(f"Test sensor: {sensor_uids[0]}")
+    yield client.get_sensor(sensor_uids[0])
 
 
 def test_sensor_first_seen(sensor):

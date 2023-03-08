8a8344e1ac3c2eb8577811d1fe7dd7201c989afc
Marius Kriegerowski <marius@gfz-potsdam.de>
2023-03-08 12:44:41 +0100

    add tests for data products

--- tests/test-client.py
+++ tests/test-client.py
@@ -1,11 +1,16 @@
 """Tests for the QuakeSaver client."""
-import datetime
 import logging
 import os
+from datetime import datetime, timedelta
 
 import pytest
 
 from quakesaver_client import QSClient, Sensor
+from quakesaver_client.models.measurement import (
+    InfluxAggregator,
+    MeasurementQuery,
+    MeasurementResult,
+)
 
 
 @pytest.fixture
@@ -33,4 +38,30 @@ def sensor(client: QSClient) -> Sensor:
 
 def test_sensor_first_seen(sensor: Sensor) -> None:
     """Test that the sensors 'first_seen' attribute is set."""
-    assert isinstance(sensor.first_seen, datetime.datetime)
+    assert isinstance(sensor.first_seen, datetime)
+
+
+@pytest.mark.parametrize("aggregator", InfluxAggregator.__dict__["__args__"])
+@pytest.mark.parametrize(
+    "query_method",
+    [
+        "get_peak_horizontal_acceleration",
+        "get_jma_intensity",
+        "get_rms_amplitude",
+        "get_spectral_intensity",
+    ],
+)
+def test_data_products(sensor: Sensor, aggregator: str, query_method: str) -> None:
+    """Test all dataproduct endpoints with all available aggregators."""
+    end_time = datetime.utcnow()
+    start_time = end_time - timedelta(minutes=30)
+
+    query = MeasurementQuery(
+        start_time=start_time,
+        end_time=end_time,
+        interval=timedelta(minutes=5),
+        aggregator=aggregator,
+    )
+    method = getattr(sensor, query_method)
+    result = method(query)
+    assert isinstance(result, MeasurementResult)
